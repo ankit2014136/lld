cab855f019a2c60ac33f82ce53d621e39c8ee508
ankitgirdhar <stanleycooper136@gmail.com>
2022-03-20 12:15:01 +0530

    repo initalize

---
+++ src/main/java/com/ankit/cachedesign/CacheDesignApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.cachedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDesignApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CacheDesignApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ankit/cachedesign/CacheDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.cachedesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheDesignApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
